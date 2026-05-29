d700587bcb783bf8e43375af92244582615c070d
abhisheksinghbhumihar <27abhishekkumar03@gmail.com>
2026-05-29 09:04:27 +0530

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,10 @@
-# API Package
+from fastapi import APIRouter
+from .endpoints import evidence
+
+router = APIRouter()
+
+router.include_router(
+    evidence.router,
+    prefix="/v1/evidence",
+    tags=["Blockchain Evidence"]
+)
