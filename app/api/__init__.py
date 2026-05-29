208513d5b8d903bf4806edd62d03b68674e3afbb
abhisheksinghbhumihar <27abhishekkumar03@gmail.com>
2026-05-29 16:23:44 +0530

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,1 @@
-from fastapi import APIRouter
-from .endpoints import fraud
-
-router = APIRouter()
-
-router.include_router(
-    fraud.router,
-    prefix="/fraud",
-    tags=["Fraud Detection"]
-)
+# API Package
